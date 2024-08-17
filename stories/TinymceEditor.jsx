300a2279990c434319af0d6459e5663724ab77a2
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-08-17 21:26:25 +0530

    ♻️ Refactored modes to global scope, updated fontSize

--- stories/TinymceEditor.jsx
+++ stories/TinymceEditor.jsx
@@ -2,8 +2,6 @@ import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import '../public/tinymce/tinymce.js'
 
-
-
 const sizes = {
   'none': {
   
@@ -22,20 +20,7 @@ const sizes = {
   }
 }
 
-export default function TinymceEditor({ skin = 'oxide', mode, value, size = 'none'}) {
-  const editorRef = useRef(null);
-
-    const commonConfiguration = {
-    skin,
-    base_url: '/tinymce',
-    promotion: false,
-    statusbar: false,
-    menubar: false,
-    highlight_on_focus: false,
-    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-      }
-
-  const modes = {
+const modes = {
     "super" : {
       plugins: [
         'advlist', 'autolink', 'lists', 'link', 'anchor',
@@ -45,6 +30,9 @@ export default function TinymceEditor({ skin = 'oxide', mode, value, size = 'non
         'bold italic forecolor | alignleft aligncenter ' +
         'alignright alignjustify | bullist numlist outdent indent | ' +
         'codeeditor | supercode',
+        supercode: {
+          fontSize: 12,
+        }
     },
     "classic" : {
       plugins: [
@@ -57,6 +45,22 @@ export default function TinymceEditor({ skin = 'oxide', mode, value, size = 'non
         'codeeditor | code',
     }
   }
+
+export default function TinymceEditor({ skin = 'oxide', mode, value, size = 'none'}) {
+  const editorRef = useRef(null);
+
+    const commonConfiguration = {
+    skin,
+    base_url: '/tinymce',
+    promotion: false,
+    statusbar: false,
+    menubar: false,
+    highlight_on_focus: false,
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+
+      }
+
+
 
   React.useEffect(() => {
     console.debug('skin reloaded');
